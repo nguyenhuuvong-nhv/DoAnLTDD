22cb33e040893d1b881fc817837de45dd9d114ec
Khoa <khoa4755@gmail.com>
2020-10-24 12:35:33 +0700

    khoa

---
+++ VongShip/app/src/main/java/com/example/vongship_android/Notifications.java
@@ -0,0 +1,65 @@
+package com.example.vongship_android;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Notifications extends AppCompatActivity {
+    ListView listView;
+    String mTitle[] = {"Chiều rồi!Trà sữa nào bạn ơi! Đồng giá 17K, freeship nữa nà",
+            "Sáng nay ăn gì, cứ lên Loship, đặt ngay thôi nào, đồng giá 10K lại free ship tận nhà",
+            " Yêu một người là khi \n Mình nghĩ về người đó \n Cứ mỗi sáng nhắn nhỏ \n Loship rồi hay chưa?",
+            "Trần Thị Huệ đã hoàn thành đơn hàng đầu tiên với mã giới thiệu của bạn. Bạn nhận được 10.000đ",
+            "Chiều nay uống gì nhỉ, lên Loship liền đồng giá 29K mà còn được freeship tận nhà nè",
+            " Nụ cười ươm tia nắng \n Ánh mắt đầy ngọt ngào \n Loship nhanh đi chớ \n Anh đang chờ sáng nay",
+            "Loship chưa thấy bạn đặt? Tặng mã THOMNGON, giảm 20.000đ khi đặt đơn hàng đầu tiên.",
+            "Tuy vô lý hết sức nhưng TẶNG mã LAMQUEN là điều không thể ngờ. Giảm 20.000đ cho đơn hàng đầu tiên nhé! Lại còn được Freeship nữa!"};
+    String mDescription[]={"4 phút","15 phút","18 phút","1 giờ","19 giờ","23 giờ","1 ngày","1 ngày"};
+    int images[] ={R.drawable.trsua,R.drawable.ansang,R.drawable.icon_loship,R.drawable.avatar_meo,R.drawable.douong,R.drawable.icon_loship,R.drawable.icon_loship,R.drawable.icon_loship};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.ListView);
+        MyAdapter adapter = new MyAdapter( this, mTitle,mDescription,images);
+        listView.setAdapter(adapter);
+    }
+    class MyAdapter extends ArrayAdapter<String> {
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+        MyAdapter (Context c, String title[],String description[],int imgs[] ){
+            super(c,R.layout.row_notifications, R.id.text,title);
+            this.context =c;
+            this.rTitle=title;
+            this.rDescription=description;
+            this.rImgs=imgs;
+        }
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row_notifications,parent, false);
+            ImageView images= row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.text);
+            TextView myDescription = row.findViewById(R.id.time);
+
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+            return row;
+        }
+    }
+}

42feecae32ac62df34844d39707250ca16cc86ff
nguyenhuuvong-nhv <71600841+nguyenhuuvong-nhv@users.noreply.github.com>
2020-10-24 11:06:08 +0700

    LTDD

---
+++ VongShip/app/src/main/java/com/example/vongship_android/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.vongship_android;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new home()).commit();
+        };
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.home:
+                            selectedFragment = new home();
+                            break;
+//                        case R.id.nofi:
+//                            selectedFragment = new activity_bookmark();
+//                            break;
+                        case R.id.profile:
+                            selectedFragment = new Profile();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
+}

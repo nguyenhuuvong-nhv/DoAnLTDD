5d9d94227f5fee44ab8f847bcbf03d83660e41c6
Võ Thành Luân <71899498+graves12321@users.noreply.github.com>
2020-10-25 23:37:54 +0700

    sua gi do

--- VongShip/app/src/main/java/com/example/vongship_android/MainActivity.java
+++ VongShip/app/src/main/java/com/example/vongship_android/MainActivity.java
@@ -6,20 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         if (savedInstanceState == null) {
--- VongShip/app/src/main/java/com/example/vongship_android/dangnhap.java
+++ VongShip/app/src/main/java/com/example/vongship_android/dangnhap.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class dangnhap extends AppCompatActivity {
-
+    private Button btdangnhap;
     private TextView tvgoidangki;
     private TextView tvgoiquenmatkhau;
 
@@ -16,9 +17,17 @@ public class dangnhap extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dangnhap);
+        btdangnhap = (Button) findViewById(R.id.dangnhap);
         tvgoidangki = (TextView)  findViewById(R.id.dangki);
         tvgoiquenmatkhau = (TextView) findViewById(R.id.forgotTv);
 
+        btdangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(dangnhap.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         tvgoidangki.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
